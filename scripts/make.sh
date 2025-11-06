895de2f122d503cb08a9394058038dbe7f3a71f2
ben <benfrost42@icloud.com>
2025-11-06 10:39:44 +0100

    make script

--- scripts/make.sh
+++ scripts/make.sh
@@ -3,5 +3,5 @@ set -e
 cd build
 make -j$(nproc 2>/dev/null || sysctl -n hw.logicalcpu)
 mkdir -p debug
-cp core/falcon debug/ 2>/dev/null
-cp core/tools0/nlxtestbench/nlxtestbench debug/ 2>/dev/null
+cp core/falcon debug/
+cp core/tools1/nlxtestbench/nlxtestbench debug/
